9a089e5bb64e17c917f44f9583cfc70f0f88ef79
b4iterdev <thaimeo200389@gmail.com>
2025-03-13 10:26:22 +0700

    debug gamepad

--- F1sh.cpp
+++ F1sh.cpp
@@ -97,26 +97,10 @@
              if (!doc["action"].isNull()) {
                const char* action = doc["action"];
                Serial.println(action);
-               if (doc["action"] == "motor")
+               if (doc["action"] == "gamepad")
                {
-                 const int c1 = doc["c1"].is<int>() ? doc["c1"].as<int>() : -1;
-                 const int c2 = doc["c2"].is<int>() ? doc["c2"].as<int>() : -1;
-                 const int c3 = doc["c3"].is<int>() ? doc["c3"].as<int>() : -1;
-                 const int c4 = doc["c4"].is<int>() ? doc["c3"].as<int>() : -1;
-                 const int c5 = doc["c5"].is<int>() ? doc["c5"].as<int>() : -1;
-                 const int c6 = doc["c6"].is<int>() ? doc["c6"].as<int>() : -1;
-                 const int c7 = doc["c7"].is<int>() ? doc["c7"].as<int>() : -1;
-                 const int c8 = doc["c8"].is<int>() ? doc["c8"].as<int>() : -1;
-               }
-               if (doc["action"] == "servo")
-               {
-                 const int s1 = doc["s1"].is<int>() ? doc["s1"].as<int>() : -1;
-                 const int s2 = doc["s2"].is<int>() ? doc["s2"].as<int>() : -1;
-                 const int s3 = doc["s3"].is<int>() ? doc["s3"].as<int>() : -1;
-                 const int s4 = doc["s4"].is<int>() ? doc["s4"].as<int>() : -1;
-                 const int s5 = doc["s5"].is<int>() ? doc["s5"].as<int>() : -1;
-                 const int s6 = doc["s6"].is<int>() ? doc["s6"].as<int>() : -1;
-                 Serial.printf("s1: %d  s2: %d  s3: %d  s4: %d  s5: %d  s6: %d\n",s1,s2,s3,s4,s5,s6);
+                 const int gamepad = doc["gamepad"];
+                 Serial.println(gamepad);
                }
                if (doc["action"] == "reboot") {
                  ESP.restart();
